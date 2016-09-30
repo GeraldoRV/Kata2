66374d763cc77e2074af6797a9b60037eac9dea4
usuario <usuario@10.22.143.56>
2016-09-30 12:24:03 +0100

    Kata 2 histograma de array version 4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,13 +5,12 @@ import java.util.HashMap;
 public class Kata2 {
 
     public static void main(String[] args) {
-        //int[] vector = {2, 1, 3, 1, 4, 6, 100, 100, 2, 100};
+        //Integer[] vector = {2, 1, 3, 1, 4, 6, 100, 100, 2, 100};
         //Double[] vector = {2.0, 3.2, 4.1, 100.5, 100.5,100.5};
         String[] vector = {"Juan", "Fran", "Pepe", "Pepe", "Pepe", "Pepe"};
 
-        Histogram histo = new Histogram(vector);
-        
-        HashMap<Object, Integer> histogram = histo.getHistogram();
+        Histogram<String> histogram = 
+                CalculateHistogram.computeHistogram(vector);
 
         for (Object key : histogram.keySet()) {
             System.out.println(key + " --> " + histogram.get(key));
